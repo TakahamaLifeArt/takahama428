f9f8d0352e64abc6cdb180530616a290cec62fdd
Ks <ks.desk@gmail.com>
2017-11-28 20:41:37 +0900

    clean

---
+++ home/common/inc/sidenavi.php
@@ -0,0 +1,43 @@
+<div class="snavi">
+	<p class="title">アイテム一覧</p>
+	<div class="box list">
+		<ul>
+			<li><a href="/items/?cat=1"><img alt="Tシャツ" src="/common/img/thumbs/item/t-shirts.png" />Ｔシャツ</a></li>
+			<li><a href="/items/?cat=3"><img alt="ポロシャツ" src="/common/img/thumbs/item/polo.png" />ポロシャツ</a></li>
+			<li><a href="/items/?cat=8"><img alt="タオル" src="/common/img/thumbs/item/towel.png" />タオル</a></li>
+			<li><a href="/items/?cat=2"><img alt="パーカー・スウェット" src="/common/img/thumbs/item/sweat.png" />パーカー・スウェット</a></li>
+			<li><a href="/items/?tag=73"><img alt="スポーツウェア" src="/common/img/thumbs/item/sports.png" />スポーツウェア</a></li>
+			<li><a href="/items/?cat=13"><img alt="ロングTシャツ" src="/common/img/thumbs/item/long-t.png" />ロングＴシャツ</a></li>
+			<li><a href="/items/?cat=6"><img alt="アウター" src="/common/img/thumbs/item/outer.png" />ブルゾン・ジャンパー</a></li>
+			<li><a href="/items/?cat=5"><img alt="レディース" src="/common/img/thumbs/item/ladys.png" />レディースウェア</a></li>
+			<li><a href="/items/?cat=9"><img alt="バッグ" src="/common/img/thumbs/item/tote-bag.png" />バッグ・トートバッグ</a></li>
+			<li><a href="/items/?cat=10"><img alt="エプロン" src="/common/img/thumbs/item/apron.png" />エプロン</a></li>
+			<li><a href="/items/?cat=14"><img alt="ベビー" src="/common/img/thumbs/item/baby.png" />ベビーウェア</a></li>
+			<li><a href="/items/?cat=16"><img alt="つなぎ" src="/common/img/thumbs/item/overall.png" />つなぎ</a></li>
+			<li><a href="/items/?cat=12"><img alt="プレゼント・グッズ" src="/common/img/thumbs/item/goods.png" />記念品・プレゼント</a></li>
+			<li><a href="/items/?cat=7"><img alt="キャップ" src="/common/img/thumbs/item/cap.png" />キャップ</a></li>
+		</ul>
+	</div>
+	<p class="title">ブランド一覧</p>
+	<div class="box list">
+		<ul>
+			<li><a href="/items/?tag=58"><img alt="Printstar" src="/common/img/thumbs/brand/printstar.png" />Printstar</a></li>
+			<li><a href="/items/?tag=59"><img alt="UnitedAthle" src="/common/img/thumbs/brand/unitedathle.png" />UnitedAthle</a></li>
+			<li><a href="/items/?tag=55"><img alt="CROSS&STTCH" src="/common/img/thumbs/brand/crossandsttch.png" />CROSS&STTCH</a></li>
+			<li><a href="/items/?tag=54"><img alt="TRUSS" src="/common/img/thumbs/brand/truss.png" />TRUSS</a></li>
+			<li><a href="/items/?tag=61"><img alt="wundou" src="/common/img/thumbs/brand/wundou.png" />wundou</a></li>
+			<li><a href="/items/?tag=60"><img alt="glimmer" src="/common/img/thumbs/brand/glimmer.png" />glimmer</a></li>
+			<li><a href="/items/?tag=67"><img alt="rucca" src="/common/img/thumbs/brand/rucca.png" />rucca</a></li>
+			<li><a href="/items/?tag=56"><img alt="BEES BEAM" src="/common/img/thumbs/brand/beesbeam.png" />BEES BEAM</a></li>
+			<li><a href="/items/?tag=57"><img alt="Seventeen VergleBee" src="/common/img/thumbs/brand/seventeen-verglebee.png" />Seventeen VergleBee</a></li>
+			<li><a href="/items/?tag=69"><img alt="Touch&GO" src="/common/img/thumbs/brand/touchandgo.png" />Touch&GO</a></li>
+			<li><a href="/items/?tag=62"><img alt="CROSS" src="/common/img/thumbs/brand/cross.png" />CROSS</a></li>
+			<li><a href="/items/?tag=68"><img alt="DALUC" src="/common/img/thumbs/brand/daluc.png" />DALUC</a></li>
+			<li><a href="/items/?tag=65"><img alt="SOWA" src="/common/img/thumbs/brand/sowa.png" />SOWA</a></li>
+			<li><a href="/items/?tag=66"><img alt="AIMY" src="/common/img/thumbs/brand/aimy.png" />AIMY</a></li>
+			<li><a href="/items/?tag=108"><img alt="GILDAN" src="/common/img/thumbs/brand/gildan.png" />GILDAN</a></li>
+			<li><a href="/items/?tag=109"><img alt="ANVIL" src="/common/img/thumbs/brand/anvil.png" />ANVIL</a></li>
+			<li><a href="/items/?tag=110"><img alt="COMFORT COLORS" src="/common/img/thumbs/brand/comfortcolors.png" />ComfortColors</a></li>
+		</ul>
+	</div>
+</div>
