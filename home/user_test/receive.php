3e317cebb4f709ce4ccc6835e48264d1fb5e9aac
Ks <ks.desk@gmail.com>
2017-12-15 22:53:01 +0900

    remove

--- home/user_test/receive.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(isset($_REQUEST['ErrCode']) && $_REQUEST['ErrCode'] != "" && $_REQUEST['ErrCode'] != null){
-	header("Location: receive_ng.php");
-} else {
-	header("Location: receive_ok.php");
-}
-?>
