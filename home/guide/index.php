5512b5640acf275dce8169e17015a078205455f5
staff2 <takahamasuzuki@gmail.com>
2018-01-16 14:37:28 +0900

    後払い注意文(1/17営業時間後アップ)

--- home/guide/index.php
+++ home/guide/index.php
@@ -1,103 +1,144 @@
 <?php
 //include $_SERVER['DOCUMENT_ROOT']."/common/inc/pageinit.php";
 ?>
-<!DOCTYPE html>
-<html>
+	<!DOCTYPE html>
+	<html>
 
-<html lang="ja">
+	<html lang="ja">
 
-<head prefix="og://ogp.me/ns# fb://ogp.me/ns/fb#  website: //ogp.me/ns/website#">
-	<meta charset="UTF-8">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="Description" content="初めてタカハマライフアートをご利用になる方や、オリジナルプリントの作成をお考えのお客様はご一読ください、ご注文方法、料金、納期などについてが記載してあります。1枚からでも安い・お急ぎ製作・印刷は東京都内のタカハマライフアート！10秒で簡単・早いオリジナルTシャツ比較お見積もりも承ります。" />
-	<meta name="keywords" content="オリジナル,Tシャツ,東京,早い,即日" />
-	<meta property="og:title" content="世界最速！？オリジナルTシャツを当日仕上げ！！" />
-	<meta property="og:type" content="article" />
-	<meta property="og:description" content="業界No. 1短納期でオリジナルTシャツを1枚から作成します。通常でも3日で仕上げます。" />
-	<meta property="og:url" content="https://www.takahama428.com/" />
-	<meta property="og:site_name" content="オリジナルTシャツ屋｜タカハマライフアート" />
-	<meta property="og:image" content="https://www.takahama428.com/common/img/header/Facebook_main.png" />
-	<meta property="fb:app_id" content="1605142019732010" />
-	<title>お支払いについて　|　オリジナルTシャツ【タカハマライフアート】</title>
-	<link rel="shortcut icon" href="/icon/favicon.ico">
-	
-	<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/css.php"; ?>
-	<link rel="stylesheet" type="text/css" href="./css/pay.css" media="screen" />
-</head>
+	<head prefix="og://ogp.me/ns# fb://ogp.me/ns/fb#  website: //ogp.me/ns/website#">
+		<meta charset="UTF-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="Description" content="初めてタカハマライフアートをご利用になる方や、オリジナルプリントの作成をお考えのお客様はご一読ください、ご注文方法、料金、納期などについてが記載してあります。1枚からでも安い・お急ぎ製作・印刷は東京都内のタカハマライフアート！10秒で簡単・早いオリジナルTシャツ比較お見積もりも承ります。" />
+		<meta name="keywords" content="オリジナル,Tシャツ,東京,早い,即日" />
+		<meta property="og:title" content="世界最速！？オリジナルTシャツを当日仕上げ！！" />
+		<meta property="og:type" content="article" />
+		<meta property="og:description" content="業界No. 1短納期でオリジナルTシャツを1枚から作成します。通常でも3日で仕上げます。" />
+		<meta property="og:url" content="https://www.takahama428.com/" />
+		<meta property="og:site_name" content="オリジナルTシャツ屋｜タカハマライフアート" />
+		<meta property="og:image" content="https://www.takahama428.com/common/img/header/Facebook_main.png" />
+		<meta property="fb:app_id" content="1605142019732010" />
+		<title>お支払いについて　|　オリジナルTシャツ【タカハマライフアート】</title>
+		<link rel="shortcut icon" href="/icon/favicon.ico">
 
-<body>
-	<header>
-		<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/header.php"; ?>
-	</header>
-	
+		<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/css.php"; ?>
+		<link rel="stylesheet" type="text/css" href="./css/pay.css" media="screen" />
+	</head>
 
-	<div id="container">
-		<div class="contents">
-			<ul class="pan hidden-sm-down">
-				<li><a href="/">オリジナルＴシャツ屋ＴＯＰ</a></li>
-				<li>お支払いについて</li>
-			</ul>
-			<div class="heading1_wrapper">
-				<h1>お支払いについて</h1>
-			</div>
+	<body>
+		<header>
+			<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/header.php"; ?>
+		</header>
 
-			<div id="payment">
-				<h2 class="heading">お支払い方法と注意点</h2>
-				<div class="inner">
-					<h3 class="heading3">銀行振込</h3>
-					<p>ご希望の納品日より2営業日前の午前中までにお振込をお願い致します。お振込確認後、商品を発送致します。<br /> （土日祝は入金確認ができないのでご注意ください）
-					</p>
-					<p>お振込み手数料は、お客様のご負担とさせていただいております。ご了承下さい。</p>
 
-					<h4>お振込口座</h4>
-					<dl class="list">
-						<dt>銀行名</dt>
-						<dd>三菱東京ＵＦＪ銀行</dd>
-						<dt>支店名</dt>
-						<dd>新小岩支店　744</dd>
-						<dt>口座種別</dt>
-						<dd>普通</dd>
-						<dt>口座番号</dt>
-						<dd>3716333</dd>
-						<dt>口座名義</dt>
-						<dd>ユ）タカハマライフアート</dd>
-					</dl>
-
-					<p class="note"><span>※</span> ご指定の期日までにご入金をお願い致します。お客様のご入金が遅れてご指定の納品日に商品の到着が間に合わなかった場合、 当方では一切責任を負いかねますのでご了承ください。
-					</p>
-					<p class="note"><span>※</span> お支払い期日を過ぎた後、再三の催促・督促にもかかわらず、何のご連絡もなくお支払いのないお客様は、法的手段を含め対応させて頂きます。 この場合に発生する手数料等の諸費用、法的手続きにかかった諸費用のすべてを、未払い代金に加算して請求致します。
-					</p>
+		<div id="container">
+			<div class="contents">
+				<ul class="pan hidden-sm-down">
+					<li><a href="/">オリジナルＴシャツ屋ＴＯＰ</a></li>
+					<li>お支払いについて</li>
+				</ul>
+				<div class="heading1_wrapper">
+					<h1>お支払いについて</h1>
 				</div>
 
-				<div class="inner">
-					<h3 class="heading3">代金引換</h3>
-					<p>代金引換手数料は、1件につき一律 &yen;800（税抜）かかります。</p>
-					<p>お支払総額(商品代+送料+代金引換手数料+消費税)を配送業者にお支払い下さい。</p>
-					<p class="note"><span>※</span> お客様のご都合でお支払件数が複数になった場合、1件につき &yen;800（税抜）を追加させていただきます。
-					</p>
-					<p class="note"><span>※</span> 代金引換便にて配達時、長期不在や受け取り拒否をされた場合、代金引換キャンセルにより送料及び配送にかかる全ての費用をご負担頂きます。
-						<br /> 金額については別途ご連絡致しますので指定口座までお振込ください。
-						<br /> ご確認が取れない場合、内容証明を送らせて頂き法的手段を取らせて頂きます。内容証明発送にかかる費用も合わせてご請求させて頂きます。
-					</p>
-				</div>
+				<div id="payment">
+					<h2 class="heading">お支払い方法と注意点</h2>
+					<div class="inner">
+						<h3 class="heading3">銀行振込</h3>
+						<p>ご希望の納品日より2営業日前の午前中までにお振込をお願い致します。お振込確認後、商品を発送致します。<br /> （土日祝は入金確認ができないのでご注意ください）
+						</p>
+						<p>お振込み手数料は、お客様のご負担とさせていただいております。ご了承下さい。</p>
 
-				<div class="inner">
-					<h3 class="heading3">クレジットカード決済</h3>
-					<p>ご希望の納品日より2営業日前の午前中までにカード決済手続きをお願い致します。カード決済確認後、商品を発送致します。<br> （土日祝は決済確認ができないのでご注意ください）
+						<h4>お振込口座</h4>
+						<dl class="list">
+							<dt>銀行名</dt>
+							<dd>三菱東京ＵＦＪ銀行</dd>
+							<dt>支店名</dt>
+							<dd>新小岩支店　744</dd>
+							<dt>口座種別</dt>
+							<dd>普通</dd>
+							<dt>口座番号</dt>
+							<dd>3716333</dd>
+							<dt>口座名義</dt>
+							<dd>ユ）タカハマライフアート</dd>
+						</dl>
 
-						<br>
-					</p>
-					<p class="note">
-						<span>※</span>ご指定の期日までにカード決済手続きをお願い致します。お客様のカード決済が遅れてご指定の納品日に商品の到着が間に合わなかった場合、<br> 当方では一切責任を負いかねますのでご了承ください。
-					</p>
-					<p class="note">
-						<span>※</span>お支払い期日を過ぎた後、再三の催促・督促にもかかわらず、何のご連絡もなくお支払いのないお客様は、法的手段を含め対応させて頂きます。<br> この場合に発生する手数料等の諸費用、法的手続きにかかった諸費用のすべてを、未払い代金に加算して請求致します。
-					</p>
-					<img src="./img/card.png" style=" width: 75%;">
-				</div>
+						<p class="note"><span>※</span> ご指定の期日までにご入金をお願い致します。お客様のご入金が遅れてご指定の納品日に商品の到着が間に合わなかった場合、 当方では一切責任を負いかねますのでご了承ください。
+						</p>
+						<p class="note"><span>※</span> お支払い期日を過ぎた後、再三の催促・督促にもかかわらず、何のご連絡もなくお支払いのないお客様は、法的手段を含め対応させて頂きます。 この場合に発生する手数料等の諸費用、法的手続きにかかった諸費用のすべてを、未払い代金に加算して請求致します。
+						</p>
+					</div>
 
-				<!--<div class="inner">
+					<div class="inner">
+						<h3 class="heading3">代金引換</h3>
+						<p>代金引換手数料は、1件につき一律 &yen;800（税抜）かかります。</p>
+						<p>お支払総額(商品代+送料+代金引換手数料+消費税)を配送業者にお支払い下さい。</p>
+						<p class="note"><span>※</span> お客様のご都合でお支払件数が複数になった場合、1件につき &yen;800（税抜）を追加させていただきます。
+						</p>
+						<p class="note"><span>※</span> 代金引換便にて配達時、長期不在や受け取り拒否をされた場合、代金引換キャンセルにより送料及び配送にかかる全ての費用をご負担頂きます。
+							<br /> 金額については別途ご連絡致しますので指定口座までお振込ください。
+							<br /> ご確認が取れない場合、内容証明を送らせて頂き法的手段を取らせて頂きます。内容証明発送にかかる費用も合わせてご請求させて頂きます。
+						</p>
+					</div>
+
+					<div class="inner">
+						<h3 class="heading3">クレジットカード決済</h3>
+						<p>ご希望の納品日より2営業日前の午前中までにカード決済手続きをお願い致します。カード決済確認後、商品を発送致します。<br> （土日祝は決済確認ができないのでご注意ください）
+
+							<br>
+						</p>
+						<p class="note">
+							<span>※</span>ご指定の期日までにカード決済手続きをお願い致します。お客様のカード決済が遅れてご指定の納品日に商品の到着が間に合わなかった場合、<br> 当方では一切責任を負いかねますのでご了承ください。
+						</p>
+						<p class="note">
+							<span>※</span>お支払い期日を過ぎた後、再三の催促・督促にもかかわらず、何のご連絡もなくお支払いのないお客様は、法的手段を含め対応させて頂きます。<br> この場合に発生する手数料等の諸費用、法的手続きにかかった諸費用のすべてを、未払い代金に加算して請求致します。
+						</p>
+						<img src="./img/card.png" style=" width: 75%;">
+					</div>
+
+					<div class="inner">
+						<h3 class="heading3">後払い</h3>
+						<p>商品の到着を確認してから後払いができる、安心・簡単な決済方法です。
+							<br>
+						</p>
+						<p>後払い手数料は一律¥300（税抜）かかります。
+							<br>
+						</p>
+						<p>後払い希望のお客様は、注文の際に「後払い希望」とお伝えください。
+							<br>
+						</p>
+						<p class="note">
+							<span>※</span>法人のお客様のみのご対応となります。
+						</p>
+
+						<h4 class="heading4">＜詳細＞</h4>
+						<ul class="ato">
+							<li>ご請求方法</li>
+							<p>請求書は原則毎月4営業日目に、MF KESSAI株式会社よりメール送信されます。</p>
+
+							<li>お支払い方法</li>
+							<p>お支払期日：　原則、商品発送日の翌月末</p>
+							<p>お支払方法：　銀行振込または口座振替</p>
+							<p>お支払先口座：　請求書に記載（MF KESSAI株式会社の名義となります）</p>
+							<p class="note"><span>※</span>銀行振込手数料はお客様でのご負担をお願いいたします。</p>
+
+							<li>注意事項</li>
+							<p>MF KESSAIはMF KESSAI株式会社<a href="https://mfkessai.co.jp/buyer/">（https://mfkessai.co.jp/buyer/）</a>が運営しております。</p>
+							<p>当サービスではMF KESSAI株式会社が、請求業務の受託及び代金債権の収受権限の付与を受け、又は、販売元から当該債権の債権譲渡を受けて、請求書の発行や代金の回収、管理を行っております。</p>
+							<p>また金額によっては審査が必要な場合があり、審査期間は一営業日です。</p>
+							<p>ご希望納期や審査の結果、別のお支払方法をご提案する可能性がございます。予めご了承ください。</p>
+							<p>請求書の発行元およびご入金先口座名義は、MF KESSAI株式会社となります。お間違えのないようご注意ください。</p>
+							<p>「MF KESSAI」でご注文の場合、領収証の発行を承っておりません。銀行での振込票（ご利用明細）または通帳の記載を支払証跡としてご利用ください。</p>
+
+							<li>お支払いに関するお問い合わせ先</li>
+							<p>support@mfkessai.co.jp</p>
+							<p class="note"><span>※</span>ご請求の内容（お宛名、明細、金額を含む）に関するお問い合わせにつきましては、タカハマライフアート宛てにご連絡ください
+						</ul>
+					</div>
+
+
+					<!--<div class="inner">
 <h3 class="heading3">コンビニ決済</h3> 
 <p>
 ご希望の納品日より2日前までにコンビニ決済手続きをお願い致します。コンビニ決済確認後、商品を発送致します。（土日祝は決済確認ができないのでご注意ください）<br>
@@ -114,85 +155,85 @@
 
 </div>  -->
 
-				<h2 class="heading" id="carriage">送料について</h2>
-				<div class="inner">
-					<h3 class="heading3">送料</h3>
-					<p>送料はお届け先1ヶ所につき、全国一律 &yen;700です。</p>
-					<p>アイテム代+プリント代が &yen;30,000以上（税抜）の場合は、お届け先1ヶ所分の料金を<span>無料</span>にさせていただきます。</p>
-					<p class="note"><span>※</span> お届け先が複数ヶ所ある場合には、1ヶ所につき￥700(税抜)を追加させていただきます。</p>
-					<p class="note"><span>※</span> 弊社使用の宅配業者以外の配送業者をご希望の場合、送料が異なる場合がございます。詳しくはスタッフまでお問い合わせ下さい。</p>
-				</div>
-				<div class="inner">
-					<h3 class="heading3">配送</h3>
-					<p>商品発送から通常1日で、お客様ご指定のお届け先に納品されます。</p>
-					<p>東京工場でお受け取りをご希望のお客様は、17：00-18：00の間にお越し下さい。</p>
-					<p class="note"><span>※</span> お届け先ご住所が、北海道、九州、沖縄、東京離島、島根隠岐郡の何れかの場合には配送に2日かかります。</p>
-					<p class="anchor">納期は、<a href="/delivery/">お届け日を調べる</a>ページですぐに分かります。</p>
-				</div>
-			</div>
-
-			<div id="cancel">
-				<h2 class="heading">キャンセル料について</h2>
-
-				<div class="plan_wrapper">
-					<h3 class="heading3">納期プランごとのキャンセル料</h3>
-					<table>
-						<thead>
-							<tr>
-								<th>納期プラン／取消日</th>
-								<th>ご注文当日18時まで</th>
-								<th>翌日午前中まで</th>
-								<th>翌日午後以降</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>通常3日仕上げ</td>
-								<td>1枚200円 ※1</td>
-								<td>ご注文金額の50％ ※2</td>
-								<td>ご注文金額の100％ ※2</td>
-							</tr>
-							<tr>
-								<td>2日仕上げ</td>
-								<td>1枚200円 ※1</td>
-								<td colspan="2" class="ac">ご注文金額の100％ ※2</td>
-							</tr>
-							<tr>
-								<td>翌日仕上げ</td>
-								<td>1枚200円 ※1</td>
-								<td colspan="2" class="ac">ご注文金額の100％ ※2</td>
-							</tr>
-							<tr>
-								<td>当日特急仕上げ</td>
-								<td colspan="3" class="ac">ご注文金額の100％ ※2</td>
-							</tr>
-						</tbody>
-					</table>
-					<p class="note"><span>※1</span> アイテム返品手数料として。なお、すでに製版が済んでいる場合、版代をいただきます。</p>
-					<p class="note"><span>※2</span> プリント作業の進捗により</p>
+					<h2 class="heading" id="carriage">送料について</h2>
+					<div class="inner">
+						<h3 class="heading3">送料</h3>
+						<p>送料はお届け先1ヶ所につき、全国一律 &yen;700です。</p>
+						<p>アイテム代+プリント代が &yen;30,000以上（税抜）の場合は、お届け先1ヶ所分の料金を<span>無料</span>にさせていただきます。</p>
+						<p class="note"><span>※</span> お届け先が複数ヶ所ある場合には、1ヶ所につき￥700(税抜)を追加させていただきます。</p>
+						<p class="note"><span>※</span> 弊社使用の宅配業者以外の配送業者をご希望の場合、送料が異なる場合がございます。詳しくはスタッフまでお問い合わせ下さい。</p>
+					</div>
+					<div class="inner">
+						<h3 class="heading3">配送</h3>
+						<p>商品発送から通常1日で、お客様ご指定のお届け先に納品されます。</p>
+						<p>東京工場でお受け取りをご希望のお客様は、17：00-18：00の間にお越し下さい。</p>
+						<p class="note"><span>※</span> お届け先ご住所が、北海道、九州、沖縄、東京離島、島根隠岐郡の何れかの場合には配送に2日かかります。</p>
+						<p class="anchor">納期は、<a href="/delivery/">お届け日を調べる</a>ページですぐに分かります。</p>
+					</div>
 				</div>
 
-				<div class="notice_wrapper">
-					<h3 class="heading3">イメージ画像のキャンセル料</h3>
-					<p>画像送付後、イメージ画像作成代1500円（画像作成の料金として）<br>＋デザイン料（必要な場合）</p>
-					<ul class="notice">
-						<li>全て税別です。</li>
-						<li>キャンセル受付日より3日以内に銀行振込にてご入金ください。</li>
-					</ul>
+				<div id="cancel">
+					<h2 class="heading">キャンセル料について</h2>
+
+					<div class="plan_wrapper">
+						<h3 class="heading3">納期プランごとのキャンセル料</h3>
+						<table>
+							<thead>
+								<tr>
+									<th>納期プラン／取消日</th>
+									<th>ご注文当日18時まで</th>
+									<th>翌日午前中まで</th>
+									<th>翌日午後以降</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>通常3日仕上げ</td>
+									<td>1枚200円 ※1</td>
+									<td>ご注文金額の50％ ※2</td>
+									<td>ご注文金額の100％ ※2</td>
+								</tr>
+								<tr>
+									<td>2日仕上げ</td>
+									<td>1枚200円 ※1</td>
+									<td colspan="2" class="ac">ご注文金額の100％ ※2</td>
+								</tr>
+								<tr>
+									<td>翌日仕上げ</td>
+									<td>1枚200円 ※1</td>
+									<td colspan="2" class="ac">ご注文金額の100％ ※2</td>
+								</tr>
+								<tr>
+									<td>当日特急仕上げ</td>
+									<td colspan="3" class="ac">ご注文金額の100％ ※2</td>
+								</tr>
+							</tbody>
+						</table>
+						<p class="note"><span>※1</span> アイテム返品手数料として。なお、すでに製版が済んでいる場合、版代をいただきます。</p>
+						<p class="note"><span>※2</span> プリント作業の進捗により</p>
+					</div>
+
+					<div class="notice_wrapper">
+						<h3 class="heading3">イメージ画像のキャンセル料</h3>
+						<p>画像送付後、イメージ画像作成代1500円（画像作成の料金として）<br>＋デザイン料（必要な場合）</p>
+						<ul class="notice">
+							<li>全て税別です。</li>
+							<li>キャンセル受付日より3日以内に銀行振込にてご入金ください。</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
 
-	<footer class="page-footer">
-		<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/footer.php"; ?>
-	</footer>
+		<footer class="page-footer">
+			<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/footer.php"; ?>
+		</footer>
 
-	<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/util.php"; ?>
+		<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/util.php"; ?>
 
-	<div id="overlay-mask" class="fade"></div>
+		<div id="overlay-mask" class="fade"></div>
 
-	<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/js.php"; ?>
-</body>
+		<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/js.php"; ?>
+	</body>
 
-</html>
+	</html>
