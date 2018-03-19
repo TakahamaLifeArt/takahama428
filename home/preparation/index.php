e08d1589588542aee8841cb5e2a40522ea5ca4a0
staff2 <takahamasuzuki@gmail.com>
2018-03-19 11:32:37 +0900

    サイトマップ

---
+++ home/preparation/index.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head prefix="og://ogp.me/ns# fb://ogp.me/ns/fb#  website: //ogp.me/ns/website#">
+	<meta charset="UTF-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="Description" content="作成が早い！Tシャツをオリジナルで作ってみませんか？サイトマップを使用して、アイテムから料金、プリントや目的に合わせたシーンで探せることができます。弊社でご注文されたお客様の作品もご確認できます。タカハマライフアートの探してる内容が見つけられますので是非ご活用ください。" />
+	<meta name="keywords" content="オリジナル,tシャツ,サイトマップ" />
+	<meta property="og:title" content="世界最速！？オリジナルTシャツを当日仕上げ！！" />
+	<meta property="og:type" content="article" />
+	<meta property="og:description" content="業界No. 1短納期でオリジナルTシャツを1枚から作成します。通常でも3日で仕上げます。" />
+	<meta property="og:url" content="https://www.takahama428.com/" />
+	<meta property="og:site_name" content="オリジナルTシャツ屋｜タカハマライフアート" />
+	<meta property="og:image" content="https://www.takahama428.com/common/img/header/Facebook_main.png" />
+	<meta property="fb:app_id" content="1605142019732010" />
+	<title>サイトマップ ｜ オリジナルTシャツが早い、タカハマライフアート</title>
+	<link rel="shortcut icon" href="/icon/favicon.ico">
+	<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/css.php"; ?>
+	<link rel="stylesheet" type="text/css" href="/common/js/lightbox/jquery.lightbox-0.5.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="./css/style.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="/items/css/items_style_responsive.css" media="screen">
+</head>
+
+<body>
+	<header>
+		<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/header.php"; ?> </header>
+
+	<div id="container">
+
+		<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/sidenavi.php"; ?>
+		<div class="contents" width="100%">
+			<ul class="pan hidden-sm-down">
+				<li><a href="/">オリジナルＴシャツ屋ＴＯＰ</a></li>
+				<li>サイトマップ</li>
+			</ul>
+
+			<div class="heading1_wrapper">
+				<h1>サイトマップ</h1>
+				<p class="comment"></p>
+			</div>
+
+			<div class="sitemap">
+				<h2>注文ナビ</h2>
+				<ul>
+					<li><a href="/order/">お申し込み</a></li>
+					<li><a href="/order/reorder.php">追加再注文</a></li>
+					<li><a href="/order/bigorder/">大口注文</a></li>
+					<li><a href="/guide/orderflow.php">注文の流れ</a></li>
+				</ul>
+				<ul>
+					<li><a href="/delivery/">お届け日を調べる</a></li>
+					<li><a href="/guide/bring.php">持ち込みについて</a></li>
+					<li><a href="/guide/faq.php">よくある質問</a></li>
+					<li><a href="/contact/">お問い合わせ</a></li>
+				</ul>
+				<ul>
+					<li><a href="/order/express/">お急ぎの方へ</a></li>
+					<li><a href="/contact/request.php">資料請求</a></li>
+				</ul>
+			</div>
+
+			<div class="sitemap">
+				<h2>料金</h2>
+				<ul>
+					<li><a href="/price/estimate.php">10秒見積もり</a></li>
+					<li><a href="/guide/discount.php">割引プラン</a></li>
+
+				</ul>
+				<ul>
+					<li><a href="/guide/">お支払いについて</a></li>
+					<li><a href="/price/fee/">プリント料金案内</a></li>
+				</ul>
+			</div>
+
+			<div class="sitemap">
+				<h2>プリントするアイテムを探す</h2>
+				<ul>
+					<li><a href="/items/category.php">プリントするアイテムを選ぶ</a></li>
+					<li><a href="/items/category/t-shirts/">Tシャツ</a></li>
+					<li><a href="/items/category/polo-shirts/">ポロシャツ</a></li>
+					<li><a href="/items/category/towel/">タオル</a></li>
+				</ul>
+				<ul>
+					<li><a href="/items/category/sweat/">パーカー・スウェット</a></li>
+					<li><a href="/items/category/sportswear/">スポーツウェア</a></li>
+					<li><a href="/items/category/long-shirts/">ロングTシャツ</a></li>
+					<li><a href="/items/category/outer/">ブルゾン・ジャンパー</a></li>
+				</ul>
+				<ul>
+					<li><a href="/items/category/ladys/">レディース</a></li>
+					<li><a href="/items/category/tote-bag/">バッグ・トートバッグ</a></li>
+					<li><a href="/items/category/apron/">エプロン</a></li>
+					<li><a href="/items/category/baby/">ベビー</a></li>
+				</ul>
+				<ul>
+					<li><a href="/items/category/overall/">つなぎ</a></li>
+					<li><a href="/items/category/workwear/">ワークウェア</a></li>
+					<li><a href="/items/category/goods/">記念品・プレゼント</a></li>
+					<li><a href="/items/category/cap/">キャップ・バンダナ</a></li>
+				</ul>
+			</div>
+			
+			<div class="sitemap">
+				<h2>取り扱いブランドについて</h2>
+				<ul>
+					<li><a href="/items/?tag=108">ギルダン</a></li>
+					<li><a href="/items/?tag=61">wundou</a></li>
+					<li><a href="/items/?tag=59">UnitedAthle</a></li>
+				</ul>
+				<ul>
+					<li><a href="/items/?tag=43">チャンピオン</a></li>
+					<li><a href="/items/?tag=58">Printstar</a></li>
+					<li><a href="/items/?tag=60">glimmer</a></li>
+				</ul>
+			</div>
+
+			<div class="sitemap">
+				<h2>プリントについて</h2>
+				<ul>
+					<li><a href="/design/printing.php">プリント方法</a></li>
+					<li><a href="/design/printsize.php">プリントサイズ</a></li>
+					<li><a href="/design/position.php">プリント位置</a></li>
+				</ul>
+				<ul>
+					<li><a href="/campaign/towel/extrabig.php">特大プリントタオル</a></li>
+					<li><a href="/campaign/towel/">最速オリジナルタオル</a></li>
+					<li><a href="/design/emb.php">刺繍</a></li>
+				</ul>
+			</div>
+			
+			<div class="sitemap">
+				<h2>デザインについて</h2>
+				<ul>
+					<li><a href="/design/template_illust.php">入稿テンプレート</a></li>
+					<li><a href="/design/designguide.php">デザインの入稿・作り方</a></li>
+					<li><a href="/design/designtemp.php">無料デザイン素材集</a></li>
+					<li><a href="/contact/faxorderform.pdf">手描きデザインシート</a></li>
+				</ul>
+				<ul>
+					<li><a href="/design/designguide_illust.php">Illustrator・Photoshopで入稿</a></li>
+					<li><a href="/design/designguide_freehand.php">手描きで入稿</a></li>
+					<li><a href="/design/designguide_image.php">画像で入稿</a></li>
+					<li><a href="/design/fontcolor.php">インク・フォント</a></li>
+				</ul>
+				<ul>
+					<li><a href="/design/gallery.php">デザイン実例</a></li>
+					<li><a href="/design/support.php">デザインサポート</a></li>
+				</ul>
+			</div>
+			<div class="sitemap">
+				<h2>シーンについて</h2>
+				<ul>
+					<li><a href="/scene/graduation/">卒業・卒園記念品</a></li>
+				</ul>
+				<ul>
+					<li><a href="/campaign/towel/noshi.php">短納期粗品タオル</a></li>
+				</ul>
+			</div>
+
+			<div class="sitemap">
+				<h2>投稿について</h2>
+				<ul>
+					<li><a href="/app/WP/thanks-blog/">お客様ブログ</a></li>
+					<li><a href="/app/WP/">スタッフブログ</a></li>
+				</ul>
+				<ul>
+					<li><a href="https://www.instagram.com/takahama_voice/">お客様インスタ画像</a></li>
+					<li><a href="/userreviews/">お客様ご利用レビュー</a></li>
+				</ul>
+			</div>
+			<div class="sitemap">
+				<h2>マイページについて</h2>
+				<ul>
+					<li><a href="/user/my_menu.php">マイページ</a></li>
+				</ul>
+			</div>
+
+			<div class="sitemap">
+				<h2>会社案内</h2>
+				<ul>
+					<li><a href="/corporate/overview.php">会社概要</a></li>
+					<li><a href="/corporate/transactions.php">特定商取引法について</a></li>
+					<li><a href="/reason/speed.php">タカハマが早い３つの理由</a></li>
+				</ul>
+				<ul>
+					<li><a href="/guide/information.php">お知らせ</a></li>
+					<li><a href="/sitemap/">サイトマップ</a></li>
+				</ul>
+			</div>
+			
+			<div class="sitemap">
+				<h2>SNSについて</h2>
+				<ul>
+					<li><a href="https://www.instagram.com/takahamalifeart/">takahamalifeart instagram</a></li>
+					<li><a href="https://www.facebook.com/takahamalifeart/">オリジナルTシャツ屋 facebook</a></li>
+				</ul>
+				<ul>
+					<li><a href="https://twitter.com/takahamalifeart">オリジナルTシャツ屋さん Twitter</a></li>
+					<li><a href="/contact/line/">LINEでお問い合わせが簡単!</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<footer class="page-footer">
+		<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/footer.php"; ?>
+	</footer>
+	<script type="text/javascript">
+		window._pt_sp_2 = [];
+		_pt_sp_2.push('setAccount,52e3170c');
+		var _protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+		(function() {
+			var atag = document.createElement('script');
+			atag.type = 'text/javascript';
+			atag.async = true;
+			atag.src = _protocol + 'js.ptengine.jp/pta.js';
+			var stag = document.createElement('script');
+			stag.type = 'text/javascript';
+			stag.async = true;
+			stag.src = _protocol + 'js.ptengine.jp/pts.js';
+			var s = document.getElementsByTagName('script')[0];
+			s.parentNode.insertBefore(atag, s);
+		})();
+
+	</script>
+
+	<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/util.php"; ?>
+	<div id="overlay-mask" class="fade"></div>
+	<?php include $_SERVER['DOCUMENT_ROOT']."/common/inc/js.php"; ?>
+	<script type="text/javascript" src="/common/js/lightbox/jquery.lightbox-0.5.js"></script>
+
+</body>
+
+</html>
