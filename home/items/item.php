e54dc3d1c10a967ad58456ff2fc3c5ed9777d4bd
staff2 <takahamasuzuki@gmail.com>
2018-03-20 15:53:31 +0900

    Revert "アイテムページ　修正"
    This reverts commit 4b37fd0c1351ddbdc88ea9ba1652ad6f1ac5a2b0.

--- home/items/item.php
+++ home/items/item.php
@@ -344,11 +344,6 @@ $_version = time();
 							<p class="note mb-1 inkjet_notice" hidden="hidden"><span class="red_mark">※</span>プリント色が生地より薄い色の場合、記載金額より高くなりますのでご了承ください</p>
 							<p class="note"><span class="red_mark">※</span>お見積もりは概算です。デザインの内容によって変更になる場合がございます。</p>
 						</div>
-						
-						<div class="btn_box flex_add">
-							<button class="btn add_btn add_print_area waves-effect waves-light"><i class="fa fa-plus mr-1" aria-hidden="true"></i> プリント箇所を追加</button>
-							<button class="hidden btn del_print_area btn-outline-danger waves-effect del_btn_2">上記プリント情報を削除</button>
-						</div>
 
 					</div>
 				</section>
@@ -398,7 +393,5 @@ $_version = time();
 	<script type="text/javascript" src="/common/js/api.js"></script>
 	<script type="text/javascript" src="/order/js/orderlib.js"></script>
 	<script type="text/javascript" src="/items/js/estimate_sole.js?v=<?php echo $_version;?>"></script>
-	<script src="/order/js/order.js?v=<?php echo $_version;?>"></script>
-	<script src="/order/js/dialog.js"></script>
 </body>
 </html>
